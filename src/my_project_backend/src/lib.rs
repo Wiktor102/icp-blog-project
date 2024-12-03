7eac7ba25e2c1942a48e87b359aaa782c512e06f
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-03 17:09:18 +0000

    Add title validation

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -13,7 +13,16 @@ fn get_articles() -> Vec<Article> {
 }
 
 #[ic_cdk::update]
-fn add_article(title: String, author: String, tags: Vec<String>, content: String) {
+fn add_article(title: String, author: String, tags: Vec<String>, content: String) -> Result<String, String> {
+    if title.len() == 0 {
+        return Err("Title cannot be empty".to_string());
+    }
+
+    if title.len() >= 64 {
+        return Err("Title is too long".to_string());
+    }
+
     let new_article = Article::new(title, author, tags, content);
     ARTICLES.with(|articles| articles.borrow_mut().push(new_article));
+    Ok("OK".to_string())
 }
