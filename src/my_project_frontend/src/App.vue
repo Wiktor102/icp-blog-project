5715ac5028d5ec27d314106e1061a65d23c1bdec
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-05 17:25:08 +0000

    Display articles nicely

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -3,13 +3,15 @@
 	import { my_project_backend } from "declarations/my_project_backend/index";
 	let articles = ref([]);
 
-    let currentTag = ref("");
-    let addedTags = ref([]);
+	let currentTag = ref("");
+	let addedTags = ref([]);
+
+    let loading = ref(false);
 
 	function makeArticle(article) {
 		return {
 			...article,
-            date: new Date(parseInt(article.date / BigInt(1_000_000))).toLocaleDateString(),
+			date: new Date(parseInt(article.date / BigInt(1_000_000))).toLocaleDateString()
 		};
 	}
 
@@ -24,31 +26,33 @@
 		const form = target.closest("form");
 		const formData = new FormData(form);
 
+        loading.value = true;
 		const response = await my_project_backend.add_article(
 			formData.get("title"),
 			formData.get("author"),
-			addedTags,
+			addedTags.value,
 			formData.get("content")
 		);
+        loading.value = false;
 
-        if (response.Err) {
-            alert(response.Err);
-            return;
-        }
+		if (response.Err) {
+			alert(response.Err);
+			return;
+		}
 
 		articles.value = [...articles.value, makeArticle(response.Ok)];
 	}
 
-    function handleTagChange (e) {
-        if (e.key !== ",") return;
-        e.preventDefault && e.preventDefault();
-        addedTags.value =  [...addedTags.value, ...e.target.value.split(",")];
-        currentTag.value = "";
-    }
+	function handleTagChange(e) {
+		if (e.key !== ",") return;
+		e.preventDefault && e.preventDefault();
+		addedTags.value = [...addedTags.value, ...e.target.value.split(",")];
+		currentTag.value = "";
+	}
 
-    function deleteTag (index) {
-        addedTags.value = addedTags.value.filter((_, i) => i !== index);
-    }
+	function deleteTag(index) {
+		addedTags.value = addedTags.value.filter((_, i) => i !== index);
+	}
 
 	getBlogs();
 </script>
@@ -62,21 +66,31 @@
 			</div>
 			<div class="group" id="author-group">
 				<label for="author">by</label>
-				<input id="author" name="author" type="text" placeholder="ex. Jack Smith"/>
+				<input id="author" name="author" type="text" placeholder="ex. Jack Smith" />
 			</div>
 			<div class="group">
 				<label for="content">Article text:</label>
 				<textarea name="content" id="content" cols="30" rows="10"></textarea>
 			</div>
-            <div id="tags">
-                <label for="tags">Tags</label>
-                <input id="tags" name="tags" type="text" :value=currentTag @keydown="handleTagChange" />
-                <ul id="selected-tags">
-                    <li v-for="(tag, index) in addedTags" :key="index" class="tag">{{ tag }} <button @click="() => deleteTag(index)">x</button></li>
-                </ul>
-            </div>
-			<button type="submit">Add</button>
+			<div id="tags">
+				<label for="tags">Tags</label>
+				<input id="tags" name="tags" type="text" :value="currentTag" @keydown="handleTagChange" />
+				<ul id="selected-tags">
+					<li v-for="(tag, index) in addedTags" :key="index" class="tag">
+						{{ tag }} <button @click="() => deleteTag(index)">x</button>
+					</li>
+				</ul>
+			</div>
+			<button type="submit" :disabled="loading">Add</button>
 		</form>
-        {{ articles }}
+		<section v-for="(article, index) in articles" :key="article.index" class="article">
+            <hr v-if="index > 0">
+            <h2>{{ article.title }}</h2>
+            <p class="author">By {{ article.author }} on {{ article.date }}</p>
+            <p class="content">{{ article.content }}</p>
+			<div class="tags">
+				{{ article.tags.join(", ")}}
+			</div>
+		</section>
 	</main>
 </template>
